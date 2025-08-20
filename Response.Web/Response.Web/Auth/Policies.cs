c50b690e709f9322c2dccac58bd35f45a99f0fe8
Luigi Marino <emails@luigi-marino.com>
2025-08-20 12:47:03 +0100

    Added role and policy helpers

---
+++ Response.Web/Response.Web/Auth/Policies.cs
@@ -0,0 +1,7 @@
+namespace Response.Web.Auth;
+
+public static class Policies
+{
+    public const string CanManageTenants = "CanManageTenants";
+    public const string CanCloseTicket = "CanCloseTicket";
+}
---
+++ Response.Web/Response.Web/Auth/RoleNames.cs
@@ -0,0 +1,8 @@
+namespace Response.Web.Auth;
+
+public static class RoleNames
+{
+    public const string Admin = "Admin";
+    public const string Agent = "Agent";
+    public const string Viewer = "Viewer";
+}
