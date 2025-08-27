1a3833dc02af14a8858f75c741b3b8d7f16be1aa
Luigi Marino <108744623+Slappie64@users.noreply.github.com>
2025-08-27 16:39:40 +0100

    Created AppDbContext and Lambda Extension

--- Response.Infrastructure/Persistence/AppDbContext.cs
+++ Response.Infrastructure/Persistence/AppDbContext.cs
@@ -1,11 +1,46 @@
 using Microsoft.EntityFrameworkCore;
+using Response.Domain.Entities;
+using Response.Infrastructure.Persistence.Extensions;
 
 namespace Response.Infrastructure.Persistence;
 
 public class AppDbContext : DbContext
 {
+
+    private readonly Guid? _currentTenantId;
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     { }
 
-    // DBSets will be added at a later time
+    public AppDbContext(DbContextOptions<AppDbContext> options, Guid currentTenantId) : base(options)
+    {
+        _currentTenantId = currentTenantId;
+    }
+
+    // Add DbSets for your entities here
+    public DbSet<Tenant> Tenants => Set<Tenant>();
+    public DbSet<AppUser> Users => Set<AppUser>();
+    public DbSet<Ticket> Tickets => Set<Ticket>();
+    public DbSet<Comment> Comments => Set<Comment>();
+    public DbSet<EmailTemplate> EmailTemplates => Set<EmailTemplate>();
+    public DbSet<TenantSequence> TenantSequences => Set<TenantSequence>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Tenant Filter
+        if (_currentTenantId.HasValue)
+        {
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (entityType.FindProperty("TenantId") != null)
+                {
+                    modelBuilder.Entity(entityType.ClrType)
+                        .HasQueryFilter(EntityTypeBuilderExtensions
+                            .BuildLambdaForTenantFilter(entityType.ClrType, _currentTenantId.Value));
+                }
+            }
+        }
+    }
 }
+
