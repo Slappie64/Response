2d14767171c2bf2f2dab2619cd1806305833b481
Luigi Marino <108744623+Slappie64@users.noreply.github.com>
2025-08-30 19:46:09 +0100

    Added EntraTenantId to Tenant.cs

--- Response.Domain/Entities/Tenant.cs
+++ Response.Domain/Entities/Tenant.cs
@@ -5,6 +5,7 @@ public class Tenant
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Domain { get; set; }
+    public Guid? EntraTenantId { get; set; }
     public ICollection<AppUser> Users { get; set; } = [];
     public ICollection<Ticket> Tickets { get; set; } = [];
 }
