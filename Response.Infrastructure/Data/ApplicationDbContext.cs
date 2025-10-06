cfa4c4cf258c0d9f21ef05592817e07acfba2bfb
Luigi Marino <108744623+Slappie64@users.noreply.github.com>
2025-10-06 21:47:00 +0100

    Made a start on AppDbContext - Got distracted

--- Response.Infrastructure/Data/ApplicationDbContext.cs
+++ Response.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,6 +1,9 @@
 namespace Response.Infrastructure.Data;
 
 public class AppDbContext : IdentityDbContext<User>
-{ 
-    
-}
+{
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+}   
